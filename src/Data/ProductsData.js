84c672f757fb62752381cb15b2c302533f4bb096
Josseling Hurtado <64708205+joseling@users.noreply.github.com>
2022-07-07 17:59:11 +0200

    Add files via upload

---
+++ src/Data/ProductsData.js
@@ -0,0 +1,45 @@
+export const ProductsData = [
+ {
+    id:1,
+    price:10,
+    amount:0,
+    name:"Gruvgear Fretwraps String Mute Small",
+    img:"https://www.doctorbass.net/imagftp/IMm1_287121576-49976_gruvgear-fretwraps-string-mute-small-x1.jpg",
+ },
+ {
+    id:2,
+    price:15,
+    amount:0,
+    name:"Correa de guitarra de poliéster ajustable para bajo eléctrico",
+    img:"https://ae01.alicdn.com/kf/Hec6069e0c8b8408a8e2ebfab01b2c455p.jpg",
+ },
+ {
+    id:3,
+    price:8,
+    amount:0,
+    name:"Auriculares para Bajo eléctrico",
+    img:"https://parabajoelectrico.com/wp-content/uploads/auriculares-para-bajo-electrico-ath-m50x-calidad.jpg",
+ },
+ {
+    id:4,
+    price:12,
+    amount:0,
+    name:"Singular Sound BeatBuddy",
+    img:"https://www.thomann.de/blog/wp-content/uploads/2015/11/nos-idees-cadeaux-bassiste23.jpg",
+ },
+ {
+    id:5,
+    price:6,
+    amount:0,
+    name:"Puas Bajo",
+    img:"https://www.betoforonda.com/wp-content/uploads/2020/09/Como-elegir-la-pua-de-bajo-correcta.jpg",
+ },
+ {
+   id:6,
+   price:6,
+   amount:0,
+   name:"Afinador para BAjo",
+   img:"https://parabajoelectrico.com/wp-content/uploads/afinador-bajo-electrico-pinza-clip-Rayzm-crom%C3%A1tico-autom%C3%A1tico.jpg",
+},
+
+]
